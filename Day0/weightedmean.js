f3b9a331b10dd80318b94cef6608177c2db42596
manthan ankolekar <manthan.ank46@gmail.com>
2022-05-13 17:49:08 +0530

    day0

---
+++ Day0/weightedmean.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'weightedMean' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY X
+ *  2. INTEGER_ARRAY W
+ */
+
+function weightedMean(X, W) {
+    // Write your code here
+    console.log((X.reduce((a,b,i)=>a+b*W[i])/W.reduce((a,b)=>a+b)).toFixed(1))
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const vals = readLine().replace(/\s+$/g, '').split(' ').map(valsTemp => parseInt(valsTemp, 10));
+
+    const weights = readLine().replace(/\s+$/g, '').split(' ').map(weightsTemp => parseInt(weightsTemp, 10));
+
+    weightedMean(vals, weights);
+}
